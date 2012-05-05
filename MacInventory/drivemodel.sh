e0d09b94ea26649072f2259431e30db1331f6d5d
Joseph Gimenez <joseph.gimenez@peoplematter.com>
2012-05-04 21:38:38 -0400

    Updated comments for drivemodel bash script to describe how function works

--- MacInventory/drivemodel.sh
+++ MacInventory/drivemodel.sh
@@ -6,4 +6,7 @@
 #  Created by joseph gimenez on 5/3/12.
 #  Copyright (c) 2012 PeopleMatter. All rights reserved.
 
+# Filters out all lines from system_profile output with "Model" string
+# Prints 2nd and 3rd columns, separated by a space (make of drive if applicable + " " + model)
+
 system_profiler SPSerialATADataType | awk '/Model:/ { print $2 " " $3; }'
