bdbeee97e13461fb25649c8212adc619f0955486
Joseph Gimenez <joseph.gimenez@peoplematter.com>
2012-05-04 21:39:58 -0400

    Updated comments for getserial bash script to describe its function

--- MacInventory/getserial.sh
+++ MacInventory/getserial.sh
@@ -6,4 +6,7 @@
 #  Created by joseph gimenez on 4/28/12.
 #  Copyright (c) 2012 PeopleMatter. All rights reserved.
 
+# searches for line with "IOPlatformSerialNumber" string
+# Parses out string at characters 37-48
+
 ioreg -l | grep IOPlatformSerialNumber | cut -c 37-48
