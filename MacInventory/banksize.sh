5bd42858aece12a65f4383803878fa446b415862
Joseph Gimenez <joseph.gimenez@peoplematter.com>
2012-05-04 21:35:08 -0400

    Updated comments for banksize script to indicate how the code works

--- MacInventory/banksize.sh
+++ MacInventory/banksize.sh
@@ -6,4 +6,9 @@
 #  Created by joseph gimenez on 4/28/12.
 #  Copyright (c) 2012 PeopleMatter. All rights reserved.
 
+# This works by returning a large amount of information about the memory installed
+# We filter out the lines where "Size" string is found
+# We then print the 2nd column (i.e., the second item using whitespace as delimiter), which is the RAM size in GB.
+
 system_profiler -detailLevel mini SPMemoryDataType | awk '/Size/ {print $2;}'
+
