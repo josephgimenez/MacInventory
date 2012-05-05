5f377d3c70f41bfdb8012e8240e00d67f426c740
Joseph Gimenez <joseph.gimenez@peoplematter.com>
2012-05-04 22:15:00 -0400

    Added comments to getos bash script to describe its function

--- MacInventory/getos.sh
+++ MacInventory/getos.sh
@@ -6,4 +6,6 @@
 #  Created by joseph gimenez on 4/28/12.
 #  Copyright (c) 2012 PeopleMatter. All rights reserved.
 
+#Filter out system_profiler output for "System Version" string
+#Print out entire line
 system_profiler -detailLevel mini SPSoftwareDataType | awk '/System Version/ { print; }'
