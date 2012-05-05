c33875270ce90327a85ed2ffcf6c333143ecbcdf
Joseph Gimenez <joseph.gimenez@peoplematter.com>
2012-05-04 21:36:38 -0400

    Updated comments for bankspeed bash code to describe code function

--- MacInventory/bankspeed.sh
+++ MacInventory/bankspeed.sh
@@ -6,4 +6,8 @@
 #  Created by joseph gimenez on 4/28/12.
 #  Copyright (c) 2012 PeopleMatter. All rights reserved.
 
+#Output memory information from system_profiler
+#Filter out all lines with "Speed" string
+#Print 2nd column of line (whitespace delimiter), which contains RAM speed in mhz
+
 system_profiler -detailLevel mini SPMemoryDataType | awk '/Speed/ {print $2;}'
