17e330e7f307232fe2e105375bcdfefe292e90bc
Joseph Gimenez <joseph.gimenez@peoplematter.com>
2012-05-04 21:41:20 -0400

    Updated comments for how mountshare bash script function works

--- MacInventory/mountshare.sh
+++ MacInventory/mountshare.sh
@@ -6,6 +6,8 @@
 #  Created by joseph gimenez on 4/29/12.
 #  Copyright (c) 2012 PeopleMatter. All rights reserved.
 
-#echo "You passed in username: $1 with password: $2"
 mkdir ~/smbshare
+
+# Mounts samba share (i.e., windows share)
+# Uses argv[0] ($1) and argv[1] as password and mounts Belarc remote share as /Users/username/smbshare locally
 mount_smbfs //$1:$2@pm-vap-01.peoplematter.com/Belarc/Belarc ~/smbshare
