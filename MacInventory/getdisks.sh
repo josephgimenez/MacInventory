6fd670e1b03f1a6962958d411bf072bb6d835c01
Joseph Gimenez <joseph.gimenez@peoplematter.com>
2012-05-04 22:17:00 -0400

    Updated comments to describe getdisks function

--- MacInventory/getdisks.sh
+++ MacInventory/getdisks.sh
@@ -6,4 +6,6 @@
 #  Created by joseph gimenez on 4/28/12.
 #  Copyright (c) 2012 PeopleMatter. All rights reserved.
 
+#Grab line that begins with 0:
+#This line shows the total disk size on column 3 (whitespace delimiter)
 diskutil list | awk '/0:/ { print $3; }'
